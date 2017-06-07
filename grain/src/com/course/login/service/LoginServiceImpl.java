ad8d66a1432f26c7fbadc46b9aeb5504e2234c94
zhangdi2014 <153813881@qq.com>
2017-06-07 08:41:29 +0800

    注册，login

--- grain/src/com/course/login/service/LoginServiceImpl.java
+++ grain/src/com/course/login/service/LoginServiceImpl.java
@@ -4,14 +4,21 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.course.entity.Login;
+import com.course.entity.User;
 import com.course.login.dao.LoginDaoImpl;
+import com.framework.Page;
 
 @Service
-@Transactional(readOnly=true)
+@Transactional
 public class LoginServiceImpl {
 	
 	@Resource
 	private LoginDaoImpl loginDaoImpl;
+	public void addLogin(Login u) {
+		// TODO Auto-generated method stub
+		this.loginDaoImpl.saveLogin(u);
+		
+	}
 	
 	@Transactional(readOnly=false)
 	public void regist(){
@@ -26,6 +33,12 @@ public class LoginServiceImpl {
 	public Login login(String name,String pwd){
 		return this.loginDaoImpl.findByNamdAndPwd(name, pwd);
 	}
+
+	public Page<Login> listLogin(int pageNum, int i, Object[] object) {
+		// TODO Auto-generated method stub
+		return this.loginDaoImpl.findLogin(pageNum,i,object);
+	}
+
 	
 	
 }
